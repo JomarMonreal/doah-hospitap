db34b13113243bc030a5d2771a992cc37201d4a9
JomarMonreal <jpmonreal2035@gmail.com>
2025-03-16 20:30:03 +0800

    feat: added nav

--- frontend-admin/src/App.js
+++ frontend-admin/src/App.js
@@ -1,12 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Appointments from './components/AppointmentComponent';
 import Messages from './components/Messages';
 
 function App() {
   return (
-    <div className="App">
-      <Messages />
-    </div>
+    <Router>
+      <div className="app">
+        {/* Navigation */}
+        <nav className="app__nav">
+          <ul className="app__nav-list">
+            <li className="app__nav-item"><Link className="app__nav-link" to="/">Appointments</Link></li>
+            <li className="app__nav-item"><Link className="app__nav-link" to="/messages">Messages</Link></li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <div className="app__content">
+          <Routes>
+            <Route path="/" element={<Appointments />} />
+            <Route path="/messages" element={<Messages />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
